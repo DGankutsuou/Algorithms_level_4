0d6bdeada0c8531db1ad8581e7c62bad4344cc52
Ayman Abourizq <aabouriz@c3r3p7.1337.ma>
2025-03-18 21:42:31 +0000

    LINUX

--- p1_number_txt.cpp
+++ p1_number_txt.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <limits.h>
+#include "inputslib.h"
 using namespace std;
 
 string	units[20] = {
@@ -32,24 +34,77 @@ string	dozens[16] = {
 	"sixty",
 	"seventy",
 	"eightty",
-	"ninety",
-	"hundred",
-	"hundreds",
-	"thousand",
-	"thousands",
-	"million",
-	"millions",
-	"billion",
-	"billions",
+	"ninety"
+};
+
+// void	handle_after_99(string text, int number)
+// {
+
+// }
+
+void	convert_number_into_text(long number)
+{
+	if (number <= 0)
+		return ;
+	if (number < 20)
+	{
+		cout << units[number] << " ";
+		return ;
+	}
+	else if (number < 100)
+	{
+		cout << dozens[number / 10 - 2] << " ";
+		number %= 10;
+	}
+	else if (number < 1000)
+	{
+		convert_number_into_text(number / 100);
+		cout << "hundered";
+		if (number >= 200)
+			cout << "s";
+		cout << " ";
+		number %= 100;
+	}
+	else if (number < 1000000)
+	{
+		convert_number_into_text(number / 1000);
+		cout << "thousand";
+		if (number >= 2000)
+			cout << "s";
+		cout << " ";
+		number %= 1000;
+	}
+	else if (number < 1000000000)
+	{
+		convert_number_into_text(number / 1000000);
+		cout << "million";
+		if (number >= 2000000)
+			cout << "s";
+		cout << " ";
+		number %= 1000000;
+	}
+	else if (number < 1000000000000)
+	{
+		convert_number_into_text(number / 1000000000);
+		cout << "billion";
+		if (number >= 2000000000)
+			cout << "s";
+		cout << " ";
+		number %= 1000000000;
+	}
+	convert_number_into_text(number);
 }
 
-string	convert_number_into_text(unsigned long number)
+int	main(void)
 {
+	long	number;
 
-}
-
-int	main()
-{
-
+	cout << "Enter a number: ";
+	cin >> number;
+	if (number > INT_MAX)
+		number = INT_MAX;
+	cout << "The number as a text is: ";
+	convert_number_into_text(number);
+	cout << endl;
 	return 0;
 }
