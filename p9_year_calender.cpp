827ea551bfebb1a10bacc4303a20f4bb576a9e77
DGankutsuou <DGankutsuou@github.com>
2025-03-20 15:32:18 +0000

    at least you can make and compile

--- p9_year_calender.cpp
+++ p9_year_calender.cpp
@@ -1,13 +1,27 @@
 #include "timelib.hpp"
+#include "inputslib.h"
 
 void	print_year_calender_header(short year)
 {
-	cout << "\n______________________________";
-	cout << "         Calender year          ";
-	cout << "______________________________" << endl;
+	cout << "\n  _________________________________\n";
+	cout << "            Calender . " << year << "            ";
+	cout << "\n  _________________________________" << endl;
+}
+
+void	print_year_calender(short year)
+{
+	print_year_calender_header(year);
+	for (int m = 1; m <= 12; m++)
+	{
+		tms::print_month_calender(m, year);
+	}
 }
 
 int	main(void)
 {
+	short	year;
+
+	year = input::read_positive_number("Enter a year: ");
+	print_year_calender(year);
 	return (0);
 }
