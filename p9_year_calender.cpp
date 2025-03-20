f0be924cad08590a332a517dc7e1c378e4e5f3fa
DGankutsuou <DGankutsuou@github.com>
2025-03-20 15:22:15 +0000

    at least you can make and compile

---
+++ p9_year_calender.cpp
@@ -0,0 +1,13 @@
+#include "timelib.hpp"
+
+void	print_year_calender_header(short year)
+{
+	cout << "\n______________________________";
+	cout << "         Calender year          ";
+	cout << "______________________________" << endl;
+}
+
+int	main(void)
+{
+	return (0);
+}
