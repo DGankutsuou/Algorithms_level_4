69986bb10821dfe84531b26ab84b7307b41b070c
Ayman Abourizq <aabouriz@e1r8p6.1337.ma>
2025-03-21 23:23:56 +0000

    LINUX

---
+++ p19_ultimate_difference_between_days.cpp
@@ -0,0 +1,45 @@
+#include "timelib.hpp"
+
+short	ultimate_difference_between_two_dates(tms::s_date date1, tms::s_date date2, bool include_last_day = false)
+{
+	short	difference = 0;
+
+	if (tms::is_date1_less_than_date2(date1, date2))
+	{
+		while (!tms::is_date1_equal_to_date2(date1, date2))
+		{
+			date1 = tms::add_one_day_to_date(date1);
+			difference++;
+		}
+		return (include_last_day ? ++difference : difference);
+	}
+	else if (tms::is_date1_less_than_date2(date2, date1))
+	{
+		while (!tms::is_date1_equal_to_date2(date2, date1))
+		{
+			date2 = tms::add_one_day_to_date(date2);
+			difference--;
+		}
+		return (include_last_day ? --difference : difference);
+	}
+	return (difference);
+}
+
+int	main(void)
+{
+	tms::s_date	date1;
+	tms::s_date	date2;
+
+	date1 = tms::read_date("Enter date 1\n");
+	cout << "Date 1: ";
+	tms::print_date(date1);
+	date2 = tms::read_date("Enter date 2\n");
+	cout << "Date 2: ";
+	tms::print_date(date2);
+	cout << endl;
+	cout << "ultimate difference between the two dates: ";
+	cout << ultimate_difference_between_two_dates(date1, date2) << endl;
+	cout << "ultimate difference between the two dates including last day: ";
+	cout << ultimate_difference_between_two_dates(date1, date2, true) << endl;
+	return (0);
+}
