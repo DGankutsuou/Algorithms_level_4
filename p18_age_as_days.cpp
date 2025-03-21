e4620b79ab991060979a08ea327c39e53ff94bab
Ayman Abourizq <aabouriz@e1r8p6.1337.ma>
2025-03-21 23:11:13 +0000

    LINUX

--- p18_age_as_days.cpp
+++ p18_age_as_days.cpp
@@ -18,27 +18,29 @@ short	difference_between_two_dates(tms::s_date date1, tms::s_date date2, bool in
 	return (difference);
 }
 
-int	main(void)
+short	get_age_as_days(tms::s_date birthday)
 {
 	time_t	t = time(0);
 	tm		*now = localtime(&t);
-	tms::s_date	date1;
-	tms::s_date	date2;
+	tms::s_date	current_date;
 
-	date1 = tms::read_date("Enter your birthday\n");
+	current_date.year = now->tm_year + 1900;
+	current_date.month = now->tm_mon + 1;
+	current_date.day = now->tm_mday;
+	return (difference_between_two_dates(birthday, current_date));
+}
+
+int	main(void)
+{
+	tms::s_date	birthday;
+
+	birthday = tms::read_date("Enter your birthday\n");
 	cout << "\n";
 	cout << "Your birthday: ";
-	tms::print_date(date1);
-	date2.year = now->tm_year + 1900;
-	date2.month = now->tm_mon + 1;
-	date2.day = now->tm_mday;
-	cout << "Current date: ";
-	tms::print_date(date2);
+	tms::print_date(birthday);
 	cout << endl;
 	cout << "You have lived ";
-	cout << difference_between_two_dates(date1, date2);
+	cout << get_age_as_days(birthday);
 	cout << " days" << endl;
-	//cout << "difference between the two dates including last day: ";
-	//cout << difference_between_two_dates(date1, date2, true) << endl;
 	return (0);
 }
