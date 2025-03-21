93d7d698155db62a48d2555ba55a557a113b2f12
Ayman Abourizq <aabouriz@c3r2p7.1337.ma>
2025-03-21 22:02:56 +0000

    LINUX

--- p17_difference_between_dates.cpp
+++ p17_difference_between_dates.cpp
@@ -1,8 +1,24 @@
 #include "timelib.hpp"
 
-short	difference_between_two_dates(tms::s_date date1, tms::s_date date2, bool include_last_day)
+short	difference_between_two_dates(tms::s_date date1, tms::s_date date2, bool include_last_day = false)
 {
-	if (tms::is_date1_less_than_date2)
+	short	difference = 0;
+
+	if (tms::is_date1_less_than_date2(date1, date2))
+	{
+		while (!tms::is_date1_equal_to_date2(date1, date2))
+		{
+			date1 = tms::add_one_day_to_date(date1);
+			difference++;
+		}
+		if (!include_last_day)
+			difference--;
+	}
+	else
+	{
+		cout << "date 1 is not less than date 2" << endl;
+	}
+	return (difference);
 }
 
 int	main(void)
@@ -17,5 +33,12 @@ int	main(void)
 	cout << "Date 2: ";
 	tms::print_date(date2);
 	cout << endl;
+	if (tms::is_date1_less_than_date2(date1, date2))
+	{
+		cout << "difference between the two dates: ";
+		cout << difference_between_two_dates(date1, date2) << endl;
+		cout << "difference between the two dates including last day: ";
+		cout << difference_between_two_dates(date1, date2, true) << endl;
+	}
 	return (0);
 }
