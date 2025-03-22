7ecb2d6ec2c3f88562b7cff8757aa1c5bd37b939
Ayman Abourizq <aabouriz@c3r2p6.1337.ma>
2025-03-22 22:36:15 +0000

    LINUX

---
+++ p59_period_length_in_days.cpp
@@ -0,0 +1,16 @@
+#include "timelib.hpp"
+
+int	period_length_in_days(tms::s_period period, bool include_last_day = false)
+{
+	return (tms::difference_between_two_dates(period.start, period.end, include_last_day));
+}
+
+int	main(void)
+{
+	tms::s_period	period;
+
+	period = tms::read_period("Enter first period\n");
+	cout << "Period length in days: " << period_length_in_days(period) << endl;
+	cout << "Period length in days with last day: " << period_length_in_days(period, true) << endl;
+	return (0);
+}
--- timelib.hpp
+++ timelib.hpp
@@ -30,6 +30,12 @@ namespace tms
 		short	year;
 	};
 
+	struct	s_period
+	{
+		s_date	start;
+		s_date	end;
+	};
+
 	bool is_leap_year(unsigned short year)
 	{
 		return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0));
@@ -646,5 +652,43 @@ namespace tms
 		else if (tms::is_date1_before_date2(date1, date2))
 			return (e_cmparation::before);
 		return (e_cmparation::after);
+	}
+
+	s_period	read_period(string message)
+	{
+		s_period	period;
+
+		cout << message;
+		period.start = tms::read_date("\nEnter start date\n");
+		period.end = tms::read_date("\nEnter end date\n");
+		cout << endl;
+		return (period);
+	}
+
+	bool	does_periods_overlap1(s_period period1, s_period period2)
+	{
+		if (!tms::is_date1_after_date2(period1.start, period2.start))
+		{
+			if (!tms::is_date1_before_date2(period1.end, period2.start))
+				return (true);
+		}
+		else
+		{
+			if (!tms::is_date1_before_date2(period2.end, period1.start))
+				return (true);
+		}
+		return (false);
+	}
+
+	bool	does_periods_overlap2(s_period period1, s_period period2)
+	{
+		e_cmparation	condition1;
+		e_cmparation	condition2;
+
+		condition1 = tms::compare_two_dates(period1.end, period2.start);
+		condition2 = tms::compare_two_dates(period1.start, period2.end);
+		if (condition1 == e_cmparation::before || condition2 == e_cmparation::after)
+			return (false);
+		return (true);
 	}
 }
