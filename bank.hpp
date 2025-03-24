00cbf79f7c6c45c4e5f94d6a04bc99cba4110199
DGankutsuou <DGankutsuou@github.com>
2025-03-24 18:12:55 +0000

    at least you can make and compile

--- bank.hpp
+++ bank.hpp
@@ -10,6 +10,7 @@
 # define FILE_NAME "data_of_clients.txt"
 # define USERS_FILE "users.txt"
 
+
 namespace bank
 {
 	enum e_main_menu_options
@@ -32,6 +33,16 @@ namespace bank
 		e_main_menu = 4
 	};
 
+	enum e_manage_users_options
+	{
+		e_show_users_list = 1,
+		e_add_new_user = 2,
+		e_delete_user = 3,
+		e_update_user_infos = 4,
+		e_find_user = 5,
+		e_go_main_menu = 6
+	};
+
 	struct s_data
 	{
 		string acount_number;
@@ -49,8 +60,9 @@ namespace bank
 		short	permissions;
 	};
 
-	void show_transactions_screen(void);
+	void show_transactions_screen(s_user t_user);
 	void show_main_menu_screen(s_user t_user);
+	void show_manage_users_screen(s_user t_user);
 
 	string read_account_number(void)
 	{
@@ -474,7 +486,7 @@ namespace bank
 
 		cout << "press any key to go back to transactions menu...";
 		cin >> back;
-		show_transactions_screen();
+		show_transactions_screen(t_user);
 	}
 
 	void back_to_main_menu(s_user t_user)
@@ -691,6 +703,197 @@ namespace bank
 		perform_transactions_option(e_transactions_options(read_transactions_option()), t_user);
 	}
 
+	s_user split_user_record(string record)
+	{
+		s_user t_user;
+		vector<string> v_user;
+
+		v_user = ft::spliter(record, "#//#");
+		t_user.username = v_user[0];
+		t_user.password = v_user[1];
+		t_user.permissions = (char)stoi(v_user[2]);
+		return (t_user);
+	}
+
+	vector<s_user> load_users_file_to_structs(string file_name)
+	{
+		vector<s_user> v_users;
+		fstream ufile;
+		string line;
+
+		ufile.open(file_name, ios::in);
+		if (ufile.is_open())
+		{
+			while (getline(ufile, line))
+			{
+				v_users.push_back(split_user_record(line));
+			}
+		}
+		else
+		{
+			cerr << "Error: Unable to open the users file" << endl;
+			exit(1);
+		}
+		if (v_users.size() == 0)
+		{
+			cerr << "Error: No users in the system" << endl;
+			exit(1);
+		}
+		return (v_users);
+	}
+
+	bool check_login(s_user &user)
+	{
+		vector<s_user> v_users;
+
+		v_users = load_users_file_to_structs(USERS_FILE);
+		for (s_user checker : v_users)
+		{
+			if (checker.username == user.username && checker.password == user.password)
+			{
+				user.permissions = checker.permissions;
+				return (true);
+			}
+		}
+		cerr << "\nIncorrect username or password, try again\n"
+			 << endl;
+		return (false);
+	}
+
+	short read_manage_users_option(void)
+	{
+		short option;
+
+		option = input::read_number_in_range(1, 6, "Enter your option ");
+		return (option);
+	}
+
+	void back_to_manage_users_menu(s_user t_user)
+	{
+		char back;
+
+		cout << "press any key to go back to manage users menu...";
+		cin >> back;
+		show_manage_users_screen(t_user);
+	}
+
+	void print_users_header(short number_of_users)
+	{
+		cout << "\n\t\t***users list (" << number_of_users << ")***\n";
+		for (int i = 0; i < 3; i++)
+		{
+			cout << "____________________";
+		}
+		cout << '\n';
+		cout << "| Username\t| Password\t| Prmissions\n";
+		for (int i = 0; i < 3; i++)
+		{
+			cout << "____________________";
+		}
+		cout << endl;
+	}
+
+	void user_line_printer(s_user &user)
+	{
+		cout << "| " << left << setw(14) << user.username;
+		cout << "| " << left << setw(14) << user.password;
+		cout << "| " << left << setw(14) << user.permissions;
+		cout << endl;
+	}
+
+	void print_users_table(void)
+	{
+		vector<s_user> v_user;
+
+		v_user = load_users_file_to_structs(USERS_FILE);
+		if (v_user.size() == 0)
+			cout << "No available clients in the system" << endl;
+		else
+		{
+			print_users_header(v_user.size());
+			for (s_user &user : v_user)
+			{
+				user_line_printer(user);
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				cout << "____________________";
+			}
+		}
+		cout << endl;
+	}
+
+	void perform_manage_users_option(e_manage_users_options option, s_user t_user)
+	{
+		switch (option)
+		{
+		case e_manage_users_options::e_show_users_list:
+			system("clear");
+			print_users_table();
+			back_to_manage_users_menu(t_user);
+			break;
+		// case e_manage_users_options::e_add_new_user:
+		// 	system("clear");
+		// 	show_add_new_users_screen();
+		// 	back_to_manage_users_menu(t_user);
+		// 	break;
+		// case e_manage_users_options::e_delete_user:
+		// 	system("clear");
+		// 	show_delete_user_screen();
+		// 	back_to_manage_users_menu(t_user);
+		// 	break;
+		// case e_manage_users_options::e_update_user_infos:
+		// 	system("clear");
+		// 	show_update_user_screen();
+		// 	back_to_manage_users_menu(t_user);
+		// 	break;
+		// case e_manage_users_options::e_find_user:
+		// 	system("clear");
+		// 	show_find_user_screen();
+		// 	back_to_manage_users_menu(t_user);
+		// 	break;
+		case e_manage_users_options::e_go_main_menu:
+			system("clear");
+			show_main_menu_screen(t_user);
+			break;
+		default:
+			exit(0);
+		}
+	}
+
+	void show_manage_users_screen(s_user t_user)
+	{
+		system("clear");
+		cout << "=====================================\n";
+		cout << "             Manage users            \n";
+		cout << "=====================================\n";
+		cout << "\t[1]: Show users list\n";
+		cout << "\t[2]: Add new user\n";
+		cout << "\t[3]: Delete user\n";
+		cout << "\t[4]: Update user infos\n";
+		cout << "\t[5]: Find user\n";
+		cout << "\t[6]: Back to main menu\n";
+		cout << "=====================================\n";
+		cout << endl;
+		perform_manage_users_option(e_manage_users_options(read_manage_users_option()), t_user);
+	}
+
+	void show_login_screen(void)
+	{
+		s_user user;
+
+		system("clear");
+		cout << "=========================\n";
+		cout << "     login screen\n";
+		cout << "=========================" << endl;
+		do
+		{
+			user.username = input::read_string("username: ");
+			user.password = input::read_string("password: ");
+		} while (!check_login(user));
+		show_main_menu_screen(user);
+	}
+
 	void perform_main_menu_option(e_main_menu_options option, s_user t_user)
 	{
 		switch (option)
@@ -722,7 +925,7 @@ namespace bank
 			break;
 		case e_main_menu_options::e_transactions:
 			system("clear");
-			show_transactions_screen();
+			show_transactions_screen(t_user);
 			break;
 		case e_main_menu_options::e_manage_users:
 			system("clear");
@@ -730,7 +933,7 @@ namespace bank
 			break;
 		case e_main_menu_options::e_exit:
 			system("clear");
-			show_exit_screen();
+			show_login_screen();
 			break;
 		default:
 			exit(0);
