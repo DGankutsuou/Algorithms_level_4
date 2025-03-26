1abc1dcb03d7e2a20e1826a4c3aefeef518bff48
Ayman Abourizq <aabouriz@c3r2p6.1337.ma>
2025-03-26 18:13:16 +0000

    LINUX

--- pr2_atm_system.cpp
+++ pr2_atm_system.cpp
@@ -1,5 +1,7 @@
 #include "inputslib.h"
+#include "libft.hpp"
 #include <vector>
+#include <fstream>
 
 #define USERS_FILE "data_of_clients.txt"
 
@@ -7,6 +9,8 @@ struct s_user
 {
 	string account_number;
 	string pin_code;
+	string name;
+	string phone;
 	double balance;
 };
 
@@ -22,6 +26,21 @@ enum e_main_menu_options
 s_user current_user;
 
 void show_login_screen(void);
+void show_main_menu_screen(void);
+
+s_user split_user_record(string record)
+{
+	s_user t_user;
+	vector<string> v_user;
+
+	v_user = ft::spliter(record, "#//#");
+	t_user.account_number = v_user[0];
+	t_user.pin_code = v_user[1];
+	t_user.name = v_user[2];
+	t_user.phone = v_user[3];
+	t_user.balance = stod(v_user[4]);
+	return (t_user);
+}
 
 vector<s_user> load_users_file_to_structs(string file_name)
 {
@@ -50,13 +69,96 @@ vector<s_user> load_users_file_to_structs(string file_name)
 	return (v_users);
 }
 
-void back_to_main_menu()
+void back_to_main_menu(void)
 {
 	char back;
 
-	cout << "press any key to go back to main menu...";
+	cout << "\npress any key to go back to main menu...";
 	cin >> back;
 	show_main_menu_screen();
+}
+
+void show_check_balance_screen(void)
+{
+	cout << "________________________________\n";
+	cout << "\tCheck balance screen\n";
+	cout << "________________________________\n";
+	cout << "Your balance is: " << current_user.balance << "$" << endl;
+}
+
+string record_user(s_user &user)
+{
+	string record;
+	string delim;
+
+	record = "";
+	delim = "#//#";
+	record += user.account_number + delim;
+	record += user.pin_code + delim;
+	record += user.name + delim;
+	record += user.phone + delim;
+	record += to_string(user.balance) + "$";
+	return (record);
+}
+
+void save_users_to_file(string file_name, vector<s_user> &v_user)
+{
+	fstream file;
+	string record;
+
+	file.open(USERS_FILE, ios::out);
+	if (file.is_open())
+	{
+		for (s_user &user : v_user)
+		{
+			record = record_user(user);
+			file << record << endl;
+		}
+		file.close();
+	}
+	else
+	{
+		cerr << "Failed to open file :(" << endl;
+		exit(1);
+	}
+}
+
+void deposit_to_user_account(vector<s_user> &v_user)
+{
+	char answer;
+	double deposit;
+
+	answer = 'n';
+	cout << "Your balance is: " << current_user.balance << "$" << endl;
+	cout << "\nHow much do you wish to deposit?\n-> ";
+	cin >> deposit;
+	for (s_user &user : v_user)
+	{
+		if (user.account_number == current_user.account_number)
+		{
+			cout << "Do you wish to perform this transaction? (Y/N)\n-> ";
+			cin >> answer;
+			if (answer == 'Y' || answer == 'y')
+			{
+				user.balance += deposit;
+				current_user.balance += deposit;
+				cout << "Deposit successed, balance now is "
+					 << user.balance << '$' << endl;
+				save_users_to_file(USERS_FILE, v_user);
+			}
+		}
+	}
+}
+
+void show_deposit_screen(void)
+{
+	vector<s_user> v_user;
+
+	cout << "________________________________\n";
+	cout << "\tCheck balance screen\n";
+	cout << "________________________________\n";
+	v_user = load_users_file_to_structs(USERS_FILE);
+	deposit_to_user_account(v_user);
 }
 
 void perform_main_menu_option(e_main_menu_options option)
@@ -65,21 +167,23 @@ void perform_main_menu_option(e_main_menu_options option)
 	{
 	case e_main_menu_options::e_quick_withdraw:
 		system("clear");
-		show_deposit_screen();
+		show_quick_withdraw_screen();
 		back_to_main_menu_menu();
 		break;
 	case e_main_menu_options::e_normal_withdraw:
 		system("clear");
-		show_withdraw_screen();
+		show_normal_withdraw_screen();
 		back_to_main_menu_menu();
 		break;
 	case e_main_menu_options::e_deposit:
 		system("clear");
-		show_total_balances_screen();
-		back_to_main_menu_menu();
+		show_deposit_screen();
+		back_to_main_menu();
 		break;
 	case e_main_menu_options::e_check_balance:
 		system("clear");
+		show_check_balance_screen();
+		back_to_main_menu();
 		break;
 	case e_main_menu_options::e_logout:
 		system("clear");
@@ -119,11 +223,11 @@ bool check_login(s_user &user)
 	vector<s_user> v_users;
 
 	v_users = load_users_file_to_structs(USERS_FILE);
-	for (s_user checker : v_users)
+	for (s_user &checker : v_users)
 	{
 		if (checker.account_number == user.account_number && checker.pin_code == user.pin_code)
 		{
-			user.balance = checker.balance;
+			current_user = checker;
 			return (true);
 		}
 	}
